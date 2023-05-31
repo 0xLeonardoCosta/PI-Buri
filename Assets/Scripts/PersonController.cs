fa0f25105bfcc23aad6f452f66ee9a1817d2e4b3
breno <leonardomcosta13@gmail.com>
2023-05-31 20:37:27 -0300

    Atividade dia 31/05
    Início do script 'Person Controller' e importação dos modelos 3D 'Buri' e 'Janaina' já riggados e o Buri com animação de andar e correr

---
+++ Assets/Scripts/PersonController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonController : MonoBehaviour
+{
+    [SerializeField] string _nome;
+    [SerializeField] float _velocidade;
+
+    void Start()
+    {
+             
+    }
+
+    void Update()
+    {
+        Andar();
+        Pular();
+        Atirar();
+        Trepar();
+    }
+
+    void Andar()
+    {
+
+    }
+
+    void Pular()
+    {
+
+    }
+
+    void Atirar()
+    {
+
+    }
+
+    void Trepar()
+    {
+
+    }
+}
