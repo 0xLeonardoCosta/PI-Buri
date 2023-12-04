e9a28b470995beebebc651f16afe0b706fa598dd
Leandro Rocha <leandrosoner0011@gmail.com>
2023-12-04 15:17:35 -0300

    frutinha com particula 04/12/13

--- Assets/Scripts/frutacoletar.cs
+++ Assets/Scripts/frutacoletar.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class frutacoletar : MonoBehaviour
 {
+    public GameObject _partColetar;
+    public MeshRenderer _meshRenderer;
+    public Collider _collider;
+
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
             if (other.gameObject.CompareTag("frutacoletar"))
         {
-            gameObject.SetActive(false);
+            StartCoroutine(Coletar());
         }
+    }
+
+    IEnumerator Coletar ()
+    {
+        _partColetar.SetActive(true);
+        _meshRenderer.enabled = false;
+        _collider.enabled = false;
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+        
+    }
+
+    void Restart ()
+    {
+
     }
 }
