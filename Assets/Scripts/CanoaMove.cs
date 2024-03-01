767c47bf512aaef29dc0ed4d55182261364465fb
0xLeonardoCosta <leonardomcosta13@gmail.com>
2024-03-01 17:43:23 -0300

    Update CanoaMove.cs

--- Assets/Scripts/CanoaMove.cs
+++ Assets/Scripts/CanoaMove.cs
@@ -16,9 +16,6 @@ public class CanoaMove : MonoBehaviour
 
     [SerializeField] Vector3 _movement;
 
-    [SerializeField] Rigidbody _rb;
-    [SerializeField] CharacterController _controller;
-
     [SerializeField] public bool _utilizarCanoa;
     [SerializeField] bool _utilizandoCanoa;
 
@@ -28,8 +25,6 @@ public class CanoaMove : MonoBehaviour
     void Start()
     {
         _player = Camera.main.GetComponent<GaaameController>()._player;
-        //_rb = GetComponent<Rigidbody>();
-        _controller = GetComponent<CharacterController>();
     }
 
     void Update()
@@ -48,26 +43,17 @@ public class CanoaMove : MonoBehaviour
         {
             _moveBuri._usandoCanoa = true;
             _moveBuri._pivotCamera = transform;
-            //Travas de movimento e pulo
             _moveBuri._variacaoVelocidadeAndar = 0;
             _moveBuri._variacaoAltura = 0;
-            //_moveBuri._gravityMultiplier = 0;
-             _moveBuri._checkGround = false;
-             _moveBuri._checkMover = false;
-            //_moveBuri.enabled = false;
-            //Controle da câmera
-            
+            _moveBuri._checkGround = false;
+            _moveBuri._checkMover = false;
 
             //Botar o Buri Sentado na canoa
-            //_player.position = transform.position;
             _player.SetParent(_buriPosCanoa);
             _player.transform.localEulerAngles = new Vector3(0,0,0);
             _player.transform.localPosition = Vector3.zero;
 
-            //transform.RotateAround(_pivot.position, Vector3.up, _velocidadeCurva * _input.x * Time.deltaTime);
-
-            //transform.Translate(Vector3.up * _velocidade * _input.y);
-            //_rb.MovePosition(transform.position + Vector3.right * _velocidade * _input.y);
+            transform.Translate(Vector3.up * _velocidade * _input.y);
             if (_input.x < 0)
             {
                 _pivot.localPosition = new Vector3(5f,0,0);
@@ -77,10 +63,11 @@ public class CanoaMove : MonoBehaviour
                 _pivot.localPosition = new Vector3(-5f,0,0);
             }
             transform.RotateAround(_pivot.position, Vector3.up, _velocidadeCurva * _input.x * Time.deltaTime);
-            //_rb.MoveRotation(_pivot.position, Vector3.up, _velocidadeCurva * _input.x * Time.deltaTime);
-
-            //_movement = new Vector3(_input.x, _controller.velocity.y, _input.y) * _velocidade * Time.deltaTime;
-            //_controller.Move(_movement);
+        }
+        else
+        {
+            _moveBuri._usandoCanoa = false;
+            _moveBuri._pivotCamera = _moveBuri.transform;
         }
     }
 
@@ -89,6 +76,11 @@ public class CanoaMove : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             _moveBuri = other.GetComponent<MoveControl>(); 
+        }
+        if (other.gameObject.CompareTag("AtracaCanoa"))
+        {
+            Debug.Log("Atracou");
+            _utilizandoCanoa = false; 
         }
     }
 }
