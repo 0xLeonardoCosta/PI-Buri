3e1d40adebf36f876bea80fc156bb662b727c8f9
Leandro Rocha <leandrosoner0011@gmail.com>
2024-04-26 16:58:03 -0300

    tentando fazer particulas dos numeros 26/04

--- Assets/LEANDRO/minijogo/HitNumeros.cs
+++ Assets/LEANDRO/minijogo/HitNumeros.cs
@@ -2,102 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HitNumeros : MonoBehaviour
+public class HitNumeros : Item2D
 {
-    //Inimigo2D _inimigo2Dd;
-    //NewInimigo2D _inimigo2D2;
-    public GameObject _partMorte;
-    public GameObject _restart;
-    public SpriteRenderer _meshe;
-    public Collider2D _collider;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public override void DestroyItem()
     {
-        //_inimigo2Dd = GetComponent<Inimigo2D>();
-        //_inimigo2D2 = GetComponent<NewInimigo2D>();
-        _collider = GetComponent<Collider2D>();
-        //_meshe = GetComponent<SpriteRenderer>();
+        StartCoroutine(DestruirTime());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DestruirTime()
     {
-
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("bala"))
-        {
-            StartCoroutine(Morte());
-            StartCoroutine(Morte2());
-        }
-
-    }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == gameObject.layer)
-        {
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.collider);
-        }
-    }
-
-    IEnumerator Morte()
-    {
-        //_inimigo2Dd._checkMove = false;
-        _partMorte.SetActive(true);
-        //_meshe.enabled = false;
-        _collider.enabled = false;
-        yield return new WaitForSeconds(0.30f);
+        Debug.Log("tomaaaaaa");
+        Textura.enabled = false;
+        PartSaida.SetActive(true);
+        yield return new WaitForSeconds(3f);
         gameObject.SetActive(false);
-        _partMorte.SetActive(false);
     }
 
-    IEnumerator Morte2()
-    {
-        //_inimigo2D2._checkMove2 = false;
-        _partMorte.SetActive(true);
-        //_meshe.enabled = false;
-        _collider.enabled = false;
-        yield return new WaitForSeconds(0.30f);
-        gameObject.SetActive(false);
-        _partMorte.SetActive(false);
-    }
-
-
-
-    IEnumerator RestartTime()
-    {
-
-        //_meshe.enabled = false;
-
-        _restart.SetActive(true);
-        yield return new WaitForSeconds(0.30f);
-        _collider.enabled = true;
-        yield return new WaitForSeconds(0.15f);
-        // _inimigo2Dd._checkMove = true;
-
-    }
-
-    IEnumerator RestartTime2()
-    {
-
-        //_meshe.enabled = false;
-
-        _restart.SetActive(true);
-        yield return new WaitForSeconds(0.30f);
-        _collider.enabled = true;
-        yield return new WaitForSeconds(0.15f);
-        //_inimigo2D2._checkMove2 = true;
-
-    }
-
-    public void Restart()
-    {
-        StartCoroutine(RestartTime());
-        StartCoroutine(RestartTime2());
-    }
 }
---
+++ Assets/Scripts/Item2D.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string _nome;
+    [SerializeField] private int _tipo;
+    [SerializeField] private int _valor;
+
+    [SerializeField] SpriteRenderer _textura;
+    [SerializeField] GameObject _partSaida;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        _textura = GetComponent<SpriteRenderer>();
+    }
+
+    public virtual void DestroyItem()
+    {
+
+    }
+
+    protected virtual void ConstroiItem()
+    {
+
+    }
+
+    //pesquisar get set C#
+    public virtual string Nome
+    {
+        get { return _nome; }  //get
+        set { _nome = value; } // set
+    }
+    public virtual int Tipo
+    {
+        get { return _tipo; }  //get
+        set { _tipo = value; } // set
+    }
+
+    public virtual int Valor
+    {
+        get { return _valor; }  //get
+        set { _valor = value; } // set
+    }
+
+    public virtual SpriteRenderer Textura
+    {
+        get { return _textura; }  //get
+        set { _textura = value; } // set
+    }
+
+    public virtual GameObject PartSaida
+    {
+        get { return _partSaida; }  //get
+        set { _partSaida = value; } // set
+    }
+
+
+
+    // Update is called once per frame
+}
