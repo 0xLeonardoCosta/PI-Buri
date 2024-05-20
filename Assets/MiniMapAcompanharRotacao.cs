61ee7bb5ef94704d7056613a9edc02560535b15b
Leonardo Costa <leonardomcosta13@gmail.com>
2024-05-20 15:53:28 -0300

    Lolla Pauzuda

---
+++ Assets/MiniMapAcompanharRotacao.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MiniMapAcompanharRotacao : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+            
+    }
+}
