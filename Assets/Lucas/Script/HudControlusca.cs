a756c4be3207095bd6e15f9e8e6fe7b12dc28869
leandrosoner0011 <leandrosoner0011@gmail.com>
2024-09-12 10:11:58 -0300

    carregamentodoMenu 12/09/24

--- Assets/Lucas/Script/HudControlusca.cs
+++ Assets/Lucas/Script/HudControlusca.cs
@@ -1,12 +1,31 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HudControlusca : MonoBehaviour
 {
+    //[SerializeField] Transform _buriMenu;
+    //[SerializeField] Transform _janainaMenu;
+    //[SerializeField] Transform _caiporaMenu;
+    [SerializeField] Transform _personagensMenu;
+    [SerializeField] Transform _logoKidMenu;
+    [SerializeField] Transform _logoBuriMenu;
+    [SerializeField] Transform _canoaMenu;
+    [SerializeField] Transform _gpMenu;
+    [SerializeField] Transform _hudCarregamentoMenu;
+    [SerializeField] Transform _aqueleTextoMenu;
+
+    [SerializeField] private Slider _barraDeProgressoInicio;
+
     [SerializeField] List<MenuControlusca> _menuControls;
+
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,12 +56,34 @@ public class HudControlusca : MonoBehaviour
 
     public void NovoJogo()
     {
-        SceneManager.LoadScene("BuriXimba");
+        this._personagensMenu.gameObject.SetActive(false);
+        this._logoBuriMenu.gameObject.SetActive(false);
+        this._logoKidMenu.gameObject.SetActive(false);
+        this._canoaMenu.gameObject.SetActive(false);
+        this._gpMenu.gameObject.SetActive(false);
+        this._aqueleTextoMenu.gameObject.SetActive(false);
+
+        this._hudCarregamentoMenu.gameObject.SetActive(true);
+        this._barraDeProgressoInicio.gameObject.SetActive(true);
+        
+
+        StartCoroutine(CarregarCenaInicio());
     }
 
     public void SairJogo()
     {
         Debug.Log("Sair do Jogo");
         Application.Quit();
+    }
+
+    private IEnumerator CarregarCenaInicio()
+    {
+        AsyncOperation asynOperation = SceneManager.LoadSceneAsync("BuriXimba");
+        while (!asynOperation.isDone)
+        {
+            this._barraDeProgressoInicio.value = asynOperation.progress;
+            yield return null;
+        }
+
     }
 }
